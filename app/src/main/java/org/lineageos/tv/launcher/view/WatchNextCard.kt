4783507ca0c05ba8a60fc4f491cad1fe33876349
trautamaki <timi.rautamaki@gmail.com>
2024-03-02 21:35:59 +0200

    Add title text for suggestions

--- app/src/main/java/org/lineageos/tv/launcher/view/WatchNextCard.kt
+++ app/src/main/java/org/lineageos/tv/launcher/view/WatchNextCard.kt
@@ -8,12 +8,14 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.tvprovider.media.tv.BasePreviewProgram
+import androidx.tvprovider.media.tv.TvContractCompat
 import coil.load
 import org.lineageos.tv.launcher.R
 
 
-open class WatchNextCard : Card {
+open class WatchNextCard : Card, View.OnFocusChangeListener {
     private val mBannerView: ImageView by lazy { findViewById(R.id.app_banner) }
+    private var mTitle: TextView? = null
 
     constructor(context: Context?) : super(context)
 
@@ -28,6 +30,7 @@ open class WatchNextCard : Card {
     init {
         stateListAnimator =
             AnimatorInflater.loadStateListAnimator(context, R.anim.app_card_state_animator)
+        setOnFocusChangeListener(this)
     }
 
     override fun inflate() {
@@ -36,12 +39,39 @@ open class WatchNextCard : Card {
 
     @SuppressLint("RestrictedApi")
     fun setInfo(info: BasePreviewProgram) {
+        // Choose correct size title for the preview
+        mTitle = when (info.posterArtAspectRatio) {
+            TvContractCompat.PreviewProgramColumns.ASPECT_RATIO_16_9 -> {
+                findViewById(R.id.title_16_9)
+            }
+            TvContractCompat.PreviewProgramColumns.ASPECT_RATIO_4_3 -> {
+                findViewById(R.id.title_16_9)
+            }
+            TvContractCompat.PreviewProgramColumns.ASPECT_RATIO_3_2 -> {
+                findViewById(R.id.title_3_2)
+            }
+            else -> {
+                findViewById(R.id.title_4_3)
+            }
+        }
+        mTitle?.visibility = View.INVISIBLE
+
         mLabel = info.title
         mBannerView.visibility = View.VISIBLE
         mLaunchIntent = info.intent
+        mTitle?.text = info.title
 
         mBannerView.load(info.posterArtUri) {
             crossfade(750)
+        }
+    }
+
+    override fun onFocusChange(v: View, hasFocus: Boolean) {
+        mTitle?.visibility = if (hasFocus) View.VISIBLE else View.INVISIBLE
+        if (hasFocus) {
+            mTitle?.postDelayed({ mTitle?.isSelected = true }, 2000)
+        } else {
+            mTitle?.isSelected = false
         }
     }
 }
