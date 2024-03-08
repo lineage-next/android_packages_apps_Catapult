41334e8b1331c9a705e6564a260c10e4069fd3b2
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-03-08 02:03:38 +0100

    Catapult: Drop down minimum SDK to 31
    Change-Id: I84bc8fa2b7a080c3973e552459a6c76ee86c0840

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "org.lineageos.tv.launcher"
-        minSdk = 34
+        minSdk = 31
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
