43d6c60169021aff452ba6a2f7edf6a3ed58ecd9
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-03-10 14:36:26 +0100

    Catapult: Add ActivityX and PreferenceX libraries to dependencies
    Change-Id: I0ad63b2a2e16b1b48b3fc8108f7fd71ed1527b3e

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,11 +57,13 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.10"))
 
     implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.activity:activity-ktx:1.7.2")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.tv:tv-foundation:1.0.0-alpha10")
     implementation("androidx.tv:tv-material:1.0.0-alpha10")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.leanback:leanback:1.2.0-alpha04")
+    implementation("androidx.preference:preference:1.2.1")
     implementation("androidx.tvprovider:tvprovider:1.0.0")
     implementation("io.coil-kt:coil:2.6.0")
 }
