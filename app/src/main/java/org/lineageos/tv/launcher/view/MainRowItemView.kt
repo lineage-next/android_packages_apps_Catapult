b5b4367f4b89af6a8a90809b9a7a90c7394972ca
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-03-10 15:25:17 +0100

    Catapult: Use JvmOverloads for MainRowItemView/ToggleChannelView
    Change-Id: I61ae1e759abc14a1dba47f405e5af1631aed04c3

--- app/src/main/java/org/lineageos/tv/launcher/view/MainRowItemView.kt
+++ app/src/main/java/org/lineageos/tv/launcher/view/MainRowItemView.kt
@@ -14,20 +14,12 @@ import androidx.leanback.widget.HorizontalGridView
 import org.lineageos.tv.launcher.R
 import org.lineageos.tv.launcher.model.MainRowItem
 
-class MainRowItemView : LinearLayout {
+class MainRowItemView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
     // Views
     private val horizontalGridView by lazy { findViewById<HorizontalGridView>(R.id.horizontal_grid) }
     private val titleView by lazy { findViewById<TextView>(R.id.title) }
-
-    constructor(context: Context?) : super(context)
-
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
-
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
-        context,
-        attrs,
-        defStyleAttr
-    )
 
     fun setData(mainRowItem: MainRowItem) {
         titleView.text = mainRowItem.label
--- app/src/main/java/org/lineageos/tv/launcher/view/ToggleChannelView.kt
+++ app/src/main/java/org/lineageos/tv/launcher/view/ToggleChannelView.kt
@@ -14,23 +14,15 @@ import androidx.appcompat.content.res.AppCompatResources
 import org.lineageos.tv.launcher.R
 import org.lineageos.tv.launcher.model.Channel
 
-class ToggleChannelView : LinearLayout {
+class ToggleChannelView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
     // Views
     private val titleView by lazy { findViewById<TextView>(R.id.title) }
     private val switch by lazy { findViewById<Switch>(R.id.state_switch) }
 
     var moving = false
     var channel: Channel? = null
-
-    constructor(context: Context) : super(context)
-
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
-
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
-        context,
-        attrs,
-        defStyleAttr
-    )
 
     init {
         inflate(context, R.layout.toggle_channel, this)
