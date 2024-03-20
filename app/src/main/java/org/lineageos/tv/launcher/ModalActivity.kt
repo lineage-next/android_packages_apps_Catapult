7670b0339628b63fd232125be8356cda94b965de
trautamaki <timi.rautamaki@gmail.com>
2024-03-20 14:00:49 +0200

    Catapult: Commonize & style modal activities

---
+++ app/src/main/java/org/lineageos/tv/launcher/ModalActivity.kt
@@ -0,0 +1,26 @@
+/*
+ * SPDX-FileCopyrightText: 2024 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.tv.launcher
+
+import android.os.Bundle
+import android.view.Gravity
+import android.view.WindowManager
+import androidx.annotation.CallSuper
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class ModalActivity(layout: Int) : AppCompatActivity(layout) {
+    @CallSuper
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val layoutParams = window.attributes.apply {
+            gravity = Gravity.TOP or Gravity.END
+            width = WindowManager.LayoutParams.WRAP_CONTENT
+            height = WindowManager.LayoutParams.MATCH_PARENT
+        }
+        window.attributes = layoutParams
+    }
+}
